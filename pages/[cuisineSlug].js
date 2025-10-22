dee4dfe177ba8ed7a23df32412159da0d76081e1
Hassan Tanweer <f2cpkfhtrf@privaterelay.appleid.com>
2025-10-22 22:44:15 +0100

    Fix syntax error in [cuisineSlug].js
    - Removed extra closing </div> tag that was causing syntax error
    - File now passes syntax check and linting
    - Should resolve Vercel build error

--- pages/[cuisineSlug].js
+++ pages/[cuisineSlug].js
@@ -243,7 +243,6 @@ export default function CuisinePage({ cuisineSlug, venues, totalVenues, editoria
                         View Details →
                       </div>
                     </div>
-                      </div>
                     </article>
                   </Link>
                 ))}
